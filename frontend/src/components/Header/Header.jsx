a62a2b309f4362b9679345276adcf2166267a4b0
Arthur Carvalho <contatocarvalhoarthur@gmail.com>
2023-01-25 13:17:33 -0300

    feat: add header e histórico

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+export function Header (){
+    return (
+            <div className="header">
+                <h2>Chat de comunicação direta</h2>
+            </div>
+)
+
+}
