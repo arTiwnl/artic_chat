915d229b4c0966aa341161878c49d8b307da72d5
victor home <contatoanacletovictor@gmail.com>
2023-01-25 21:25:27 -0300

    25/01/23 - Connection structure started

--- backend/main.go
+++ backend/main.go
@@ -2,57 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"github.com/gorilla/websocket"
+
+	"github.com/TutorialEdge/realtime-chat-go-react/pkg/websocket"
 )
-
-var upgrader = websocket.Upgrader {
-	ReadBufferSize: 1024,
-	WriteBufferSize: 1024,
-
-	// verificando se há conexao
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-// define a rota que ira ouvir as mensagens 
-
-func reader(conn *websocket.Conn) {
-	for {
-	// lendo as mensagens
-	messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return 
-		} 
-		fmt.Println(string(p))
-
-			if err := conn.WriteMessage(messageType, p); err != nil {
-				log.Println(err)
-				return
-			}
-
-	}
-}
 
 // definindo o endpoint da lib websocket
 
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	// atualizando o websocket e a conexao
-
-	ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := websocket.Upgrade(w, r)
 	if err != nil {
-		log.Println(err)
+		fmt.Fprintf(w, "%+V\n", err)
 	}
-	reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w,"Servidor simples")
-	})
 	// mapeando o endpoint /ws para a funçao a seguir
 	http.HandleFunc("/ws", serveWs)
 }
